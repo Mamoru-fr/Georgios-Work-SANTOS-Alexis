67d06453f9d9831d5f8d56c8fc9768908ad3fa46
SANTOS Alexis <alexis.santos@algosup.com>
2025-01-13 10:11:04 +0100

    update

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ using namespace std;
 int main()
 {
     // Use smart pointers for automatic memory management
-    unique_ptr<Student> Alexis = make_unique<Student>("Alexis SANTOS", 18, 1, 13.65); // Add Alexis as a Student
-    unique_ptr<Student> Gregory = make_unique<Student>("Grégory PAGNOUX", 21, 3, 14.43); // Add Grégory as a Student
-    unique_ptr<Student> Laurent = make_unique<Student>("Laurent BOUQUIN", 23, 5, 16.89); // Add Laurent as a Student
-    unique_ptr<Student> Lena = make_unique<Student>("Léna DE GERMAIN", 18, 1, 12.5); // Add Léna as a Student
-    unique_ptr<Student> Matteo = make_unique<Student>("Mattéo LEFIN", 19, 2, 14.2); // Add Matteo as a Student
+    unique_ptr<Student> Jean = make_unique<Student>("Jean PATRICK", 18, 1, 13.65); // Add Alexis as a Student
+    unique_ptr<Student> Gregoire = make_unique<Student>("Grégoire THOMAS", 21, 3, 14.43); // Add Grégory as a Student
+    unique_ptr<Student> Ulysse = make_unique<Student>("Ulysse BAGANTIN", 23, 5, 16.89); // Add Laurent as a Student
+    unique_ptr<Student> Laurenne = make_unique<Student>("Laurenne GAMINA", 18, 1, 12.5); // Add Léna as a Student
+    unique_ptr<Student> Baptiste = make_unique<Student>("Baptiste BOULANGER", 19, 2, 14.2); // Add Matteo as a Student
 
     // Vector of unique pointers to Student objects
-    vector<unique_ptr<Student>> Students = {move(Alexis), move(Matteo), move(Lena), move(Laurent), move(Gregory)};
+    vector<unique_ptr<Student>> Students = {move(Jean), move(Baptiste), move(Laurenne), move(Ulysse), move(Gregoire)};
 
     // Print the list of students
     for (const auto& student : Students) {
